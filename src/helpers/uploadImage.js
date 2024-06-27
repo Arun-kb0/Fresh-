c364259fa9890a7d06d6c6b4168f8659967f8aad
Arun <arun11.kb@gmail.com>
2024-06-27 18:37:14 +0530

    image croping added

--- src/helpers/uploadImage.js
+++ src/helpers/uploadImage.js
@@ -1,12 +1,28 @@
 const { firebaseApp } = require("../config/firebaseConfig")
 const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage')
 const { format } = require('date-fns')
-
+const sharp = require('sharp')
+const CustomError = require('../constants/CustomError')
 
 const storage = getStorage(firebaseApp)
 
 const uploadImageToFirebase = async (file, directory) => {
   try {
+    console.log(file)
+
+    const processedBuffer = await sharp(file.buffer)
+      .resize({
+        width: 420,
+        height: 420,
+        fit: 'cover',
+      })
+      .toBuffer()
+
+    if (!Buffer.isBuffer(processedBuffer)) {
+      throw new CustomError("invalid buffer ")
+    }
+    file.buffer = processedBuffer
+
     const dateTime = Date.now()
     const formattedDate = format(dateTime, 'dd-MM-yyyy-HH:mm:ss')
     const fileName = `${file.originalname}-${formattedDate}`
