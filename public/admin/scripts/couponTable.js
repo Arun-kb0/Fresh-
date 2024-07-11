6075693963ba48584f6c031205c01437d7f38d40
Arun <arun11.kb@gmail.com>
2024-07-11 14:38:11 +0530

    delete coupon feature added

--- public/admin/scripts/couponTable.js
+++ public/admin/scripts/couponTable.js
@@ -1,6 +1,9 @@
 $(function () {
   const startDateTd = $(".startDateTd");
   const endDateTd = $(".endDateTd");
+  const deleteBtn = $(".deleteCouponBtn")
+
+  deleteBtn.on("click",handleDelete)
 
   startDateTd.each(function () {
     const dateValue = new Date($(this).text());
@@ -13,5 +16,31 @@ $(function () {
     const formattedDate = dateValue.toDateString().slice(0, 10); 
     $(this).text(formattedDate); 
   });
+
+  function handleDelete() {
+    const code = $(this).attr('data-item')
+
+    $.ajax({
+      url: '/admin/coupon',
+      method: "DELETE",
+      data: { code },
+      success: function (data) {
+        if (data) {
+          showAlert(data.message)
+        } else {
+          console.log("invalid res data")
+        }
+      },
+      error: function (xhr, status, error) {
+        const res = JSON.parse(xhr.responseText)
+        showAlert(res.message)
+        console.log(error)
+      }
+    })
+  }
+
+
+
+
 
 })
--- src/controllers/admin/couponController.js
+++ src/controllers/admin/couponController.js
@@ -6,7 +6,7 @@ const couponModel = require("../../model/couponModel")
 
 
 const getCouponsPageController = async (req, res, next) => {
-  const { page=1 } = req.query
+  const { page = 1 } = req.query
   try {
     const LIMIT = 6
     const startIndex = (Number(page) - 1) * LIMIT
@@ -15,12 +15,12 @@ const getCouponsPageController = async (req, res, next) => {
 
     const coupons = await couponModel.find({ isDeleted: false })
       .sort({ createdAt: -1 }).skip(startIndex).limit(LIMIT)
-    
+
     res.render('admin/coupon/couponTable', {
       ...viewAdminPage,
       coupons,
       numberOfPages,
-      page : Number(page)
+      page: Number(page)
     })
 
   } catch (error) {
@@ -41,12 +41,12 @@ const createCouponController = async (req, res, next) => {
   const file = req.file
   try {
     if (!file) {
-      throw new CustomError('image required to create coupon',BAD_REQUEST)
+      throw new CustomError('image required to create coupon', BAD_REQUEST)
     }
 
-    const isExists = await couponModel.findOne({code: couponData.code})
+    const isExists = await couponModel.findOne({ code: couponData.code , isDeleted:false })
     if (isExists) {
-      throw new CustomError('coupon already exists ',CONFLICT)
+      throw new CustomError('coupon already exists ', CONFLICT)
     }
     const image = await uploadImageToFirebase(file, 'coupons')
     const newCoupon = await couponModel.create({
@@ -54,7 +54,7 @@ const createCouponController = async (req, res, next) => {
       ...couponData
     })
 
-    res.status(OK).json({message:"coupon created ", coupon:newCoupon })
+    res.status(OK).json({ message: "coupon created ", coupon: newCoupon })
   } catch (error) {
     next(error)
   }
@@ -63,8 +63,18 @@ const createCouponController = async (req, res, next) => {
 
 
 const deleteCouponController = async (req, res, next) => {
+  const { code } = req.body
   try {
-
+    const result = await couponModel.findOneAndUpdate(
+      { code },
+      {
+        $set: {
+          isDeleted: true
+        }
+      }
+    )
+    console.log(result)
+    res.status(OK).json({ message: `coupon ${code} deleted` })
   } catch (error) {
     next(error)
   }
