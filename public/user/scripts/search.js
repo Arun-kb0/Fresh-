df43d0df191642ee04fd60f236eeea981c4f57d8
Arun <arun11.kb@gmail.com>
2024-08-06 00:15:50 +0530

    search for smaller screens added  & navbar updated

--- public/user/scripts/search.js
+++ public/user/scripts/search.js
@@ -3,8 +3,20 @@ $(function () {
   const searchInput = $("#searchInput")
   const searchBtn = $("#searchBtn")
 
+  const searchInputSm = $("#searchInputSm")
+  const searchBtnSm = $("#searchBtnSm")
+  
+
+  searchBtnSm.on("click", handleSearchSm)
+  searchInputSm.on("keypress", function (event) {
+    if (event.which === 13) { 
+      handleSearchSm();
+    }
+  });
+
   searchBtn.on("click", handleSearch)
   searchInput.on("keypress", function (event) {
+    const searchQuery = searchInput.val().trim()
     if (event.which === 13) { 
       handleSearch();
     }
@@ -12,6 +24,11 @@ $(function () {
   
   function handleSearch() {
     const searchQuery = searchInput.val().trim()
+    window.location.href = `/products/search?searchQuery=${searchQuery}`
+  }
+  
+  function handleSearchSm() {
+    const searchQuery = searchInputSm.val().trim()
     window.location.href = `/products/search?searchQuery=${searchQuery}`
   }
 
