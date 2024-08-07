a02e21319cc7e3a5fdce4ff5177c984b9f9f54d5
Arun <arun11.kb@gmail.com>
2024-08-08 00:17:15 +0530

    time format added for ledgerBook page

--- public/admin/scripts/ledgerBook.js
+++ public/admin/scripts/ledgerBook.js
@@ -1,5 +1,6 @@
 $(function () {
   
+  const createdAt = $(".createdAt")
   const prevBtn = $("#prevBtn")
   const nextBtn = $("#nextBtn")
 
@@ -32,4 +33,21 @@ $(function () {
     }
   }
   // * pagination end
+
+
+  // * createdAt to readable date string
+  createdAt.each(function () {
+    const element = $(this)
+    const elementValue = element.text().trim()
+    const date = new Date(elementValue)
+    const day = date.toDateString().split(' ')[0];
+    const month = date.toDateString().split(' ')[1];
+    const dateOfMonth = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+
+    const formattedDate = `${day} ${month} ${dateOfMonth} ${year} ${hour}hr:${minute}min`;
+    element.text(formattedDate)
+  })
 })
